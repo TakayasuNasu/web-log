dc94ddc53ac7247ebefdc72d62f5f1519f6f4d8a
takayasu.nasu <taka.beckham@gmail.com>
2023-01-19 05:59:00 +0000

    Modify shared component hierarchy

--- app/root.tsx
+++ app/root.tsx
@@ -10,16 +10,30 @@ import {
   useLoaderData,
 } from "@remix-run/react"
 
+// models
+import { client } from "~/models/contentful.server"
+import { getThemeSession } from "~/utils/theme.server"
+
 // context provider
 import { ThemeProvider, useTheme, ThemeHead } from "~/utils/theme-provider"
 import { AppContextProvider } from "~/context/store"
 
-import { getThemeSession } from "~/utils/theme.server"
+// components
+import Header, { links as headerLinks } from "~/components/header"
+import Nav, { MobileNav, links as navLinks } from "~/components/nav"
+import Sidebar, { links as sidebarLinks } from "~/components/sidebar"
+
+// style
+import styles from "~/styles/style.css"
 
 export const loader = async ({ request }: LoaderArgs) => {
   const themeSession = await getThemeSession(request)
+  const {
+    collection: { hashtags },
+  } = await client.getHashtagBy()
 
   return json({
+    hashtags: hashtags,
     theme: themeSession.getTheme(),
   })
 }
@@ -30,8 +44,18 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 })
 
+export function links() {
+  return [
+    ...headerLinks(),
+    ...navLinks(),
+    ...sidebarLinks(),
+    { rel: "stylesheet", href: styles },
+  ]
+}
+
 function App() {
   const data = useLoaderData<typeof loader>()
+  const { hashtags } = data
   const [theme] = useTheme()
 
   return (
@@ -42,7 +66,16 @@ function App() {
         <ThemeHead ssrTheme={Boolean(data.theme)} />
       </head>
       <body>
-        <Outlet />
+        <div data-container>
+          <Nav {...{ hashtags }} />
+
+          <main>
+            <Header />
+            <Outlet />
+          </main>
+          <Sidebar />
+        </div>
+        <MobileNav {...{ hashtags }} />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,67 +8,36 @@ import { getPostWithOgpData } from "~/models/open-graph.server"
 import type { Post } from "~/models/contentful.server"
 
 // components
-import Header, { links as headerLinks } from "~/components/header"
-import Nav, { MobileNav, links as navLinks } from "~/components/nav"
-import Sidebar, { links as sidebarLinks } from "~/components/sidebar"
 import Status, { links as statusLinks } from "~/components/status"
 
-// style
-import styles from "~/styles/style.css"
-
 export function links() {
-  return [
-    ...headerLinks(),
-    ...navLinks(),
-    ...sidebarLinks(),
-    ...statusLinks(),
-    { rel: "stylesheet", href: styles },
-  ]
+  return [...statusLinks()]
 }
 
 export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url)
   const tag = url.searchParams.get("tag")
-  console.log({ tag })
-  const siteMasta = await client.getSiteMasta()
-  console.log(siteMasta)
-  const {
-    collection: { hashtags },
-  } = await client.getHashtagBy()
-
   const posts = await client.getPosts(tag)
 
   return json({
-    hashtags: hashtags,
     posts: await getPostWithOgpData(posts),
   })
 }
 
 export default function Index() {
-  const { hashtags, posts } = useLoaderData<typeof loader>()
+  const { posts } = useLoaderData<typeof loader>()
 
   return (
     <>
-      <div data-container>
-        <Nav {...{ hashtags }} />
-
-        <main>
-          <Header />
-
-          <ul className="statuses">
-            {posts?.map((post: Post, i) => {
-              return (
-                <li key={i}>
-                  <Status {...post} />
-                </li>
-              )
-            })}
-          </ul>
-        </main>
-
-        <Sidebar />
-      </div>
-      <MobileNav {...{ hashtags }} />
+      <ul className="statuses">
+        {posts?.map((post: Post, i) => {
+          return (
+            <li key={i}>
+              <Status {...post} />
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
