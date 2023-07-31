f3eb636a7fe942d097c61efa0995f4fca754fec7
takayasu.nasu <taka.beckham@gmail.com>
2023-07-31 04:40:40 +0000

    Modify Server Build Path

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "netlify",
+  serverBuildPath: ".netlify/functions-internal/server.js",
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? "./server.js"
