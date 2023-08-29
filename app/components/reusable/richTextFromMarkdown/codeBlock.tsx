b3aff594adad46e2018c7fd056e636bcb4d57ec8
takayasu.nasu <taka.beckham@gmail.com>
2023-08-29 05:55:28 +0000

    Show file name if there is

--- app/components/reusable/richTextFromMarkdown/codeBlock.tsx
+++ app/components/reusable/richTextFromMarkdown/codeBlock.tsx
@@ -1,19 +1,26 @@
 import React from "react"
 import type { FC } from "react"
 import SyntaxHighlighter from "react-syntax-highlighter"
-import { atelierDuneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs"
+import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs"
 
 const CodeBlock: FC<{ children: React.ReactElement }> = ({
   children,
 }): JSX.Element => {
   const { className, children: code } = children.props
 
-  const language = className?.replace("lang-", "")
+  const language = className?.replace("lang-", "")?.split(":")
 
   return (
-    <SyntaxHighlighter language={language} style={atelierDuneDark}>
-      {code}
-    </SyntaxHighlighter>
+    <div className="code-block-container">
+      {language?.at(1) && (
+        <div className="code-block-filename-containe">
+          <span className="code-block-filename">{language.at(1)}</span>
+        </div>
+      )}
+      <SyntaxHighlighter language={language?.at(0)} style={atomOneDark}>
+        {code}
+      </SyntaxHighlighter>
+    </div>
   )
 }
 
